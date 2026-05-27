1ed858323df86df12d5301841ce4c2ad8041ae00
basota2 <134119058+basota2@users.noreply.github.com>
2026-05-27 19:06:51 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
-TOKEN = "ТВОЙ_ТОКЕН"
+TOKEN = 8911200848:AAHLhoQb_qTurzik46fdY_39LnDQpy119Tg
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
