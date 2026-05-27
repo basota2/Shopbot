10682beb91f18486dc4a2d68858e72078e87331f
basota2 <134119058+basota2@users.noreply.github.com>
2026-05-27 19:01:06 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,19 +1,41 @@
-import asyncio
-import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "ТВОЙ_ТОКЕН"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🛒 Товары")],
+        [KeyboardButton(text="💰 Оплата")],
+        [KeyboardButton(text="📞 Поддержка")]
+    ],
+    resize_keyboard=True
+)
+
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("🛍 Магазин работает!")
+    await message.answer(
+        "🛍 Добро пожаловать в магазин!",
+        reply_markup=menu
+    )
+
+@dp.message()
+async def buttons(message: types.Message):
+    if message.text == "🛒 Товары":
+        await message.answer("📦 Пока товаров нет")
+
+    elif message.text == "💰 Оплата":
+        await message.answer("💳 Оплата скоро будет")
+
+    elif message.text == "📞 Поддержка":
+        await message.answer("@твойдс")
 
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
